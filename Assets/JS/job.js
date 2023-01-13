e83734b091f5159b8b6644cf08b50d951dd9cdd2
NEO <qj82189@gmail.com>
2023-01-12 22:55:09 -0500

    Added to modal and jobs API to job.js and added modal and buttons to  job.html

---
+++ Assets/JS/job.js
@@ -0,0 +1,66 @@
+document.addEventListener('DOMContentLoaded', () => {
+  // Functions to open and close a modal
+  function openModal($el) {
+    $el.classList.add('is-active');
+  }
+
+  function closeModal($el) {
+    $el.classList.remove('is-active');
+  }
+
+  function closeAllModals() {
+    (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+      closeModal($modal);
+    });
+  }
+
+  // Add a click event on buttons to open a specific modal
+  (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+    const modal = $trigger.dataset.target;
+    const $target = document.getElementById(modal);
+
+    $trigger.addEventListener('click', () => {
+      openModal($target);
+    });
+  });
+
+  // Add a click event on various child elements to close the parent modal
+  (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+    const $target = $close.closest('.modal');
+
+    $close.addEventListener('click', () => {
+      closeModal($target);
+    });
+  });
+
+  // Add a keyboard event to close all modals
+  document.addEventListener('keydown', (event) => {
+    const e = event || window.event;
+
+    if (e.keyCode === 27) { // Escape key
+      closeAllModals();
+    }
+  });
+});
+
+
+function getApi() {
+
+var searchResult = $("#city-name").val()    
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd008772996mshe87e44680e82ae0p13a9ddjsne94019017c96',
+		'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+	}
+};
+
+fetch('https://jsearch.p.rapidapi.com/search?query=' + searchResult + '&num_pages=1', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+    .catch(err => console.error(err));
+
+}
+
+$("#modal-button").on("click" , getApi)
